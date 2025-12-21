9c0b96c0e68bacec2451860f10838baa00f4ccee
Sarah <s.fawzy97@yahoo.com>
2025-12-21 18:58:02 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { FooterComponent } from "./shared/layouts/footer/footer.component";
 
 // #region agent log
 try {
@@ -14,7 +15,7 @@ try {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
