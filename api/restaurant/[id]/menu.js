a0cf68b0cb6555e569b313dec022ddacb1f29cef
Sarah <s.fawzy97@yahoo.com>
2026-02-25 06:49:45 +0200

    .

--- api/restaurant/[id]/menu.js
+++ api/restaurant/[id]/menu.js
@@ -8,6 +8,5 @@ export default async function handler(req, res) {
     res.status(200).json(data);}
     catch(err){
       res.status(500).json({ error: `Failed to fetch restaurant ${id}` });
-    }
-  }
+    }}
   
