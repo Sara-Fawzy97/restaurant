833617dde2041711b330e8dcabd0ba051e3eb5cc
Sarah <s.fawzy97@yahoo.com>
2026-02-25 06:05:47 +0200

    .

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 export default async function handler(req, res) {
+  try{
   const response = await fetch(
     'https://fakerestaurantapi.runasp.net/api/Restaurant'
   );
 
   const data = await response.json();
-  res.status(200).json(data);
+  res.status(200).json(data);}
+  catch(err){
+    res.status(500).json({ error: 'Failed to fetch restaurants' });
+  }
 }
--- api/restaurant/[id]/index.js
+++ api/restaurant/[id]/index.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
     try{const { id } = req.query; // Vercel يمرر الـ params
     const response = await fetch(
-      `https://fakerestaurantapi.runasp.net/api/Restaurant/${id}/menu`
+      `https://fakerestaurantapi.runasp.net/api/Restaurant/${id}`
     );
   
     const data = await response.json();
     res.status(200).json(data);}
     catch(err){
-      res.status(500).json({ error: `Failed to fetch restaurant ${id}` });
+        res.status(500).json({ error: `Failed to fetch restaurant ${id}` });
     }
   }
   
